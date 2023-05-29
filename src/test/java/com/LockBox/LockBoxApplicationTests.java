06b452f91bd0cf89201a730cc35c80b878795ba2
Sdiabate1337 <diabatesekou1337@gmail.com>
2023-05-29 04:27:18 -0700

    auth process finished

---
+++ src/test/java/com/LockBox/LockBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LockBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
